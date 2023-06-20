4c78f7c5253d70c7c0a5f63c83d6740b756ca080
Slothy Boi <eric20wang.wang@gmail.com>
2023-06-20 05:28:52 -0700

    added almost-working save button next to pic

--- assets/develop/script.js
+++ assets/develop/script.js
@@ -11,14 +11,36 @@ function fetchCatImage() {
     fetch('https://api.thecatapi.com/v1/images/search')
     .then(resp => resp.json())
     .then(json => image.src = json[0].url);
+
+    return image;
 }
 
-function btnClick() {
-    let button = document.getElementById("new-cat-btn");
-    button.addEventListener("click", fetchCatImage);
+var currentImage = fetchCatImage();
+
+function picBtnClick() {
+    let picButton = document.getElementById("new-cat-btn");
+    picButton.addEventListener("click", fetchCatImage);
 }
+
+function processImage() {
+    var imgSource = currentImage.getAttribute("src");
+    localStorage.setItem("recent-image", imgSource);
+}
+
+function displayRecentImage() {
+    const recentImageSrc = localStorage.getItem("recent-image");
+    if (recentImageSrc) {
+        document.querySelector("#img-preview").setAttribute("src", recentImageSrc);
+    }
+}
+
+document.querySelector("#save-button").addEventListener("click", processImage);
+document.querySelector("#save-button").addEventListener("click", displayRecentImage);
+
+// code for save button and associated functions inspired by this youtube video: https://www.youtube.com/watch?v=8K2ihr3NC40
 
 document.addEventListener("DOMContentLoaded", () => {
     fetchCatImage();
-    btnClick();
+    picBtnClick();
+    displayRecentImage();
 })
